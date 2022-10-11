7675f64024c1e07db874b21026f9bc490f709c34
Omer Karakus <omer.karakus@lonres.com>
2022-10-11 14:07:06 +0100

    add g element

--- src/components/molecules/Template/index.tsx
+++ src/components/molecules/Template/index.tsx
@@ -1,7 +1,12 @@
 import * as e from "./styles";
 
 const Template = () => {
-  return <e.Wrapper id="editor-template"></e.Wrapper>;
+  return (
+    <e.Svg id="editor-template">
+      <e.G id="shapes"></e.G>
+      <e.G id="handlers"></e.G>
+    </e.Svg>
+  );
 };
 
 export default Template;
--- src/components/molecules/Template/styles.ts
+++ src/components/molecules/Template/styles.ts
@@ -1,10 +1,14 @@
 import styled from "styled-components";
 import { template } from "../../../utils/constants";
 
-export const Wrapper = styled.svg`
+export const Svg = styled.svg`
   height: ${template.height}px;
   width: ${template.width}px;
   border: 1px solid;
   border-color: lightgray;
   background-color: white;
+`;
+
+export const G = styled.g`
+  transform: translate(${template.width / 2}px, ${template.height / 2}px);
 `;
