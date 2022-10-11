04ba5da1f2d7be61fc18868b3ab3fcec74b64aa8
Omer Karakus <omer.karakus@lonres.com>
2022-10-11 14:07:35 +0100

    change D3Selection type

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -5,6 +5,6 @@ export interface DivProps extends React.HTMLAttributes<HTMLDivElement> {
   callback?: Function;
 }
 
-export type D3Selection = d3.Selection<SVGGElement, unknown, HTMLElement, any>;
+export type D3Selection = d3.Selection<d3.BaseType, unknown, HTMLElement, any>;
 
 export type Size = { width: number; height: number };
