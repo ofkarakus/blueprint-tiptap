8272f7729ac74930a985a6dfb3314432e09e315d
Omer Karakus <omer.karakus@lonres.com>
2022-10-13 15:53:18 +0100

    update initial state

--- src/components/atoms/Store/Global/duck.ts
+++ src/components/atoms/Store/Global/duck.ts
@@ -1,6 +1,5 @@
 import * as types from "./types";
 
 export const initialState: types.InitialState = {
-  shapes: [],
-  handlers: [],
+  textBlocks: [],
 };
--- src/components/atoms/Store/Global/types.ts
+++ src/components/atoms/Store/Global/types.ts
@@ -1,16 +1,5 @@
+import { ReactNode } from "react";
+
 export type InitialState = {
-  shapes: Array<d3.Selection<SVGRectElement, unknown, HTMLElement, any>>;
-  handlers: Array<
-    d3.Selection<
-      SVGCircleElement,
-      {
-        id: string;
-        class: string;
-        x: number;
-        y: number;
-      },
-      d3.BaseType,
-      unknown
-    >
-  >;
+  textBlocks: ReactNode[];
 };
