8a09c4b16cf54d26632a457f8a9f9df85223449d
Omer Karakus <omer.karakus@lonres.com>
2022-10-13 16:59:14 +0100

    remove onClick

--- src/components/molecules/Template/index.tsx
+++ src/components/molecules/Template/index.tsx
@@ -1,15 +1,10 @@
-import { removeHandler } from "../../../utils/helpers";
 import { useTrackedState } from "../../atoms/Store/Global";
 import * as e from "./styles";
 
 const Template = () => {
   const state = useTrackedState();
 
-  return (
-    <e.Wrapper id="editor-template" onClick={removeHandler}>
-      {state.textBlocks}
-    </e.Wrapper>
-  );
+  return <e.Wrapper id="editor-template">{state.textBlocks}</e.Wrapper>;
 };
 
 export default Template;
