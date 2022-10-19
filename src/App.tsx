5f317557033930410c4a603d8e5037d3997b0218
Omer Karakus <omer.karakus@lonres.com>
2022-10-19 14:10:46 +0100

    refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import { GlobalStateProvider } from "components/atoms/Store/Global";
 import Body from "components/organisms/Body";
-import React from "react";
 
 function App() {
   return (
-    <React.StrictMode>
-      <GlobalStateProvider>
-        <Body />
-      </GlobalStateProvider>
-    </React.StrictMode>
+    <GlobalStateProvider>
+      <Body />
+    </GlobalStateProvider>
   );
 }
 
