5aad21fdfa722205342f31cf26365da61668dfd3
Omer Karakus <omer.karakus@lonres.com>
2022-10-19 12:44:59 +0100

    refactoring

--- src/components/atoms/Resizable/index.tsx
+++ src/components/atoms/Resizable/index.tsx
@@ -1,9 +1,9 @@
-import { Props } from "react-rnd";
 import { textBlockStyle, textBlockDefault } from "./utils";
 import * as e from "./styles";
 import { useStore } from "utils/hooks";
+import { ResizableProps } from "./types";
 
-const Resizable = (props: Props & { type?: string, id?: number }) => {
+const Resizable = (props: ResizableProps) => {
   const { focusedBlockId } = useStore()
 
   switch (props.type) {
--- src/components/atoms/Resizable/types.ts
+++ src/components/atoms/Resizable/types.ts
@@ -1,3 +1,7 @@
+import { Props } from "react-rnd";
+
 export type StyledRndProps = {
   $isFocused: boolean;
 };
+
+export type ResizableProps = Props & { type?: string; id?: number };
