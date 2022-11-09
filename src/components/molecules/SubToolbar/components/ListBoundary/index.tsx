3a12196f6c8caf7b1a084bbf4a1bf52d76f267e3
Omer Karakus <omerfarukkarakus01@gmail.com>
2022-11-09 16:10:12 +0000

    add tool template for ListBoundary

---
+++ src/components/molecules/SubToolbar/components/ListBoundary/index.tsx
@@ -0,0 +1,12 @@
+import * as e from '../../styles';
+import LabelSizeCoords from '../LabelSizeCoords';
+
+const ListBoundary = () => {
+    return (
+        <e.Table>
+            <LabelSizeCoords />
+        </e.Table>
+    );
+};
+
+export default ListBoundary;
--- src/components/molecules/SubToolbar/index.tsx
+++ src/components/molecules/SubToolbar/index.tsx
@@ -1,5 +1,6 @@
 import Background from './components/Background';
 import DynamicImage from './components/DynamicImage';
+import ListBoundary from './components/ListBoundary';
 import StaticImage from './components/StaticImage';
 import TextBlock from './components/TextBlock';
 import * as e from './styles';
@@ -17,6 +18,8 @@ const SubToolbar = () => {
                         return <DynamicImage />;
                     case true:
                         return <Background />;
+                    case true:
+                        return <ListBoundary />;
                 }
             })()}
         </e.Wrapper>
