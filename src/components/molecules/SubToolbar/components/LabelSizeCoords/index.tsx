75a1767f66be7145d34963bd238dad8eb9e5dfd7
Omer Karakus <omerfarukkarakus01@gmail.com>
2022-11-24 14:22:10 +0000

    add LabelInput

--- src/components/molecules/SubToolbar/components/LabelSizeCoords/index.tsx
+++ src/components/molecules/SubToolbar/components/LabelSizeCoords/index.tsx
@@ -1,6 +1,7 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTag, faCompress, faArrowsAlt } from '@fortawesome/free-solid-svg-icons';
 import { useStore } from 'utils/hooks';
+import { LabelInput } from './styles';
 
 const LabelSizeCoords = () => {
     const { focusedBlock } = useStore();
@@ -12,7 +13,9 @@ const LabelSizeCoords = () => {
                     <FontAwesomeIcon icon={faTag} />
                     Label:
                 </td>
-                <td colSpan={2}>{focusedBlock?.label}</td>
+                <td colSpan={2}>
+                    <LabelInput value={focusedBlock?.label} />
+                </td>
             </tr>
             <tr>
                 <td>
---
+++ src/components/molecules/SubToolbar/components/LabelSizeCoords/styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const LabelInput = styled.input`
+    border: 0;
+    padding: 0;
+    &:focus-visible {
+        outline: none;
+    }
+`;
