2a2126878b9a92ea5a322286a70926b6468509f7
Omer Karakus <omerfarukkarakus01@gmail.com>
2023-01-06 10:42:19 +0000

    remove redux provider

--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux';
-import { store } from 'context/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
     <React.StrictMode>
-        <Provider store={store}>
-            <App />
-        </Provider>
+        <App />
     </React.StrictMode>,
 );
 
